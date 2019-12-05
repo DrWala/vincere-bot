90f65c2fbcc60d7c4bffb52482bc1dc7534a7879
Azeem Vasanwala <azeem.iron@gmail.com>
2019-12-06 00:36:37 +0800

    Initial

---
+++ bot.js
@@ -0,0 +1,72 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ActivityHandler } = require("botbuilder");
+const request = require("request");
+
+class EchoBot extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        let allPossibleResponses = [];
+
+        // Initial load
+        console.log("INFO: Reading data from API");
+        request(
+            "https://sheetsu.com/apis/v1.0su/7e0eaa894b3d",
+            { json: true },
+            (err, res, body) => {
+                if (err) {
+                    return console.log(err);
+                }
+                allPossibleResponses = body;
+                console.log("INFO: Data read complete");
+            }
+        );
+
+        // Timeout to read every 5 minutes
+        setInterval(function() {
+            console.log("INFO: Reading data from API");
+            request(
+                "https://sheetsu.com/apis/v1.0su/7e0eaa894b3d",
+                { json: true },
+                (err, res, body) => {
+                    if (err) {
+                        return console.log(err);
+                    }
+                    allPossibleResponses = body;
+                    console.log("INFO: Data read complete");
+                }
+            );
+        }, 300000);
+
+        this.onMessage(async (context, next) => {
+            let msg = context._activity.text;
+            console.log(`Processing: ${msg}`);
+
+            let thisResp  = allPossibleResponses.map(obj => obj[msg]);
+            await context.sendActivity(thisResp[Math.floor(Math.random() * thisResp.length)]);
+
+            console.log(thisResp);
+            if(thisResp[0] === undefined) {
+                await context.sendActivity('Alas! I cannot comprehend the words of a fool.');
+            }
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity("Hello and welcome!");
+                }
+            }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+}
+
+module.exports.EchoBot = EchoBot;
