f0e76b0646e3661ec7a5fae37905792b540226de
Azeem Vasanwala <azeem.iron@gmail.com>
2020-12-20 18:42:33 +0800

    Test

--- bot.js
+++ bot.js
@@ -8,33 +8,33 @@ class EchoBot extends ActivityHandler {
     constructor() {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        let allPossibleResponses = [];
+        let command_map = {};
 
         // Initial load
         console.log("INFO: Reading data from API");
         request(
-            "https://sheetsu.com/apis/v1.0su/7e0eaa894b3d",
+            "https://api.airtable.com/v0/appw8rrMigzB8sp9N/Commands?api_key=keyvYRqsOQcalWZ8O",
             { json: true },
             (err, res, body) => {
                 if (err) {
                     return console.log(err);
                 }
-                allPossibleResponses = body;
+                command_map = this.formatAirtableResponse(body);
                 console.log("INFO: Data read complete");
             }
         );
 
         // Timeout to read every 5 minutes
-        setInterval(function() {
+        setInterval(function () {
             console.log("INFO: Reading data from API");
             request(
-                "https://sheetsu.com/apis/v1.0su/7e0eaa894b3d",
+                "https://api.airtable.com/v0/appw8rrMigzB8sp9N/Commands?api_key=keyvYRqsOQcalWZ8O",
                 { json: true },
                 (err, res, body) => {
                     if (err) {
                         return console.log(err);
                     }
-                    allPossibleResponses = body;
+                    command_map = this.formatAirtableResponse(body);
                     console.log("INFO: Data read complete");
                 }
             );
@@ -44,19 +44,18 @@ class EchoBot extends ActivityHandler {
             let msg = context._activity.text;
             console.log(`Processing: ${msg}`);
 
-            if(msg.indexOf('@') !== '-1' || msg.substring(0, msg.indexOf('@sir_vincere_bot') !== '')) {
-
-                if(!msg.indexOf('@') !== '-1') {
-                    msg = msg.substring(0, msg.indexOf('@sir_vincere_bot'))
-                }
-                let thisResp = allPossibleResponses
-                    .map(obj => obj[msg])
-                    .filter(obj => obj !== undefined)
-                    .filter(str => str !== "");
+            // Valid messages to allow:
+            // should contain @sir_vincere_bot
+            if (
+                msg.indexOf("@") !== "-1" &&
+                msg.substring(0, msg.indexOf("@sir_vincere_bot")) !== ""
+            ) {
+                msg = msg.substring(0, msg.indexOf("@sir_vincere_bot"));
+                let thisResp = command_map[msg];
                 await context.sendActivity(
                     thisResp[Math.floor(Math.random() * thisResp.length)]
                 );
-    
+
                 console.log(thisResp);
                 if (thisResp[0] === undefined) {
                     await context.sendActivity(
@@ -64,6 +63,11 @@ class EchoBot extends ActivityHandler {
                     );
                 }
                 // By calling next() you ensure that the next BotHandler is run.
+                await next();
+            } else if (msg == "@sir_vincere_bot") {
+                await context.sendActivity("Yes you called?");
+                await next();
+            } else {
                 await next();
             }
         });
@@ -78,6 +82,21 @@ class EchoBot extends ActivityHandler {
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    formatAirtableResponse(resp) {
+        let command_map = {};
+        for (let index = 0; index < resp.records.length; index++) {
+            const record = resp.records[index].fields;
+            let name = record["Name"];
+            command_map[name] = [];
+            for (const [key, value] of Object.entries(record)) {
+                if (key != "Name") {
+                    command_map[name].push(value);
+                }
+            }
+        }
+        return command_map;
     }
 }
 
